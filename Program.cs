50112f689950c1bb75a17a0aff54e9fc1f9ad4ea
Andrew Virts <asvirts@outlook.com>
2024-01-30 14:21:50 -0600

    playing with classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var otto = new Pet("Otto");
+
+Console.WriteLine(otto.Name);
+
+public class Pet
+{
+    private string pet;
+
+    public Pet(string pet)
+    {
+        this.pet = pet;
+    }
+
+    public string Name { get; set; }
+}
